235fd00d92f86c396a6879cdd6cbe2f8b927f5dc
Dylan Palmboom <nofxboy1234@gmail.com>
2022-11-28 21:07:45 +1000

    Print counts for substrings > 0

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
-def substrings(_word, dictionary)
+def substrings(word, dictionary)
+  words_found = dictionary.to_h { |item| [item, 0] }
   dictionary.each do |w|
-    puts w
+    words_found[w] += 1 if word.include? w.downcase
   end
-  # s.include? 'Bel'.downcase
+  words_found.select { |_key, value| value.positive? }
 end
 
-substrings('a', %w[a b c])
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+puts substrings('below', dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
 # Search for each substring in word (case-insensitive)
 # For each substring found
 #   Add it to a hash with substring as key and count as value
