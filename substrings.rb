a0290adaed8b7d424aff7d688abfd40973ea2536
Dylan Palmboom <nofxboy1234@gmail.com>
2022-11-29 12:05:59 +1000

    Init rspec in project

--- substrings.rb
+++ substrings.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
-def substrings(word, dictionary)
-  words_found = dictionary.to_h { |item| [item, 0] }
-  dictionary.each do |w|
-    words_found[w] += 1 if word.include? w.downcase
-  end
-  words_found.select { |_key, value| value.positive? }
-end
-
-dictionary = %w[below down go going horn how howdy it i low own part partner sit]
-puts substrings('below', dictionary)
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
 # Search for each substring in word (case-insensitive)
 # For each substring found
 #   Add it to a hash with substring as key and count as value
 # Return the count hash
+
+def substrings
+  'hello'
+end
+
+# def substrings(word, dictionary)
+#   words_found = dictionary.to_h { |item| [item, 0] }
+#   dictionary.each do |w|
+#     puts w
+#     words_found[w] = word.downcase.count(w.downcase)
+#   end
+#   puts words_found
+#   words_found.select { |_key, value| value.positive? }
+# end
+
+# dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+# substrings('below', dictionary)
 
 # > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 # => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
