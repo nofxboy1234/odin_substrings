d5b6660e14571371465f84d0269e4741e1b082b0
Dylan Palmboom <nofxboy1234@gmail.com>
2022-11-30 12:57:51 +1000

    Run rspec tests - 1 left

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -2,31 +2,15 @@
 
 require 'pry-byebug'
 
-# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
-# Search for each substring in word (case-insensitive)
-# For each substring found
-#   Add it to a hash with substring as key and count as value
-# Return the count hash
-
-# def substrings(num_a, num_b)
-# binding.pry
-#   num_a + num_b
-# end
-
 def substrings(word, dictionary)
+  # binding.pry
   words_found = dictionary.to_h { |item| [item, 0] }
   dictionary.each do |w|
-    # puts w
-    words_found[w] += 1 if word.downcase.include?(w.downcase)
+    offset = 0
+    while word.index(w, offset)
+      words_found[w] += 1
+      offset = word.index(w, offset) + 1
+    end
   end
-  # puts words_found
   words_found.select { |_key, value| value.positive? }
 end
-
-# dictionary = %w[below down go going horn how howdy it i low own part partner sit]
-# substrings('below', dictionary)
-
-# > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# > substrings("below", dictionary)
-# => { "below" => 1, "low" => 1 }
--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -9,4 +9,12 @@ describe 'substrings method' do
                         partner sit]
         expect(substrings('below', dictionary)).to eql({ 'below' => 1, 'low' => 1 })
       end
+
+  it 'handles multiple words' do
+    dictionary = %w[below down go going horn how howdy it i low own part
+                    partner sit]
+    expect(substrings("Howdy partner, sit down! How's it going?",
+                      dictionary)).to eql({ 'down' => 1, 'go' => 1, 'going' => 1, 'how' => 2, 'howdy' => 1, 'it' => 2,
+                                            'i' => 3, 'own' => 1, 'part' => 1, 'partner' => 1, 'sit' => 1 })
+  end
 end
